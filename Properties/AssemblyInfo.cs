a4911ecd2652a35f6c149d446479ded2b15c6926
Dale Fugier <dale@mcneel.com>
2017-11-02 13:35:55 -0700

    Initial .NET commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Rhino.PlugIns;
+
+// Plug-in Description Attributes
+[assembly: PlugInDescription(DescriptionType.Address, "3670 Woodland Park Avenue North\r\nSeattle, WA 98103")]
+[assembly: PlugInDescription(DescriptionType.Country, "United States")]
+[assembly: PlugInDescription(DescriptionType.Email, "dale@mcneel.com")]
+[assembly: PlugInDescription(DescriptionType.Phone, "206-545-6877")]
+[assembly: PlugInDescription(DescriptionType.Fax, "206-545-7321")]
+[assembly: PlugInDescription(DescriptionType.Organization, "Robert McNeel & Associates")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "https://github.com/dalefugier/RhinoFileVersion")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "http://www.rhino3d.com/")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rhino 3DM File Version")]
+
+// This will be used also for the plug-in description.
+[assembly: AssemblyDescription("Rhino 3DM File Version utility")]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Robert McNeel & Associates")]
+[assembly: AssemblyProduct("RhinoFileVersion")]
+[assembly: AssemblyCopyright("Copyright © 2014, Robert McNeel & Associates")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aa091cb2-d8c0-4944-a17d-b66c80bec4ca")] // This will also be the Guid of the Rhino plug-in
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
+
+// Make compatible with Rhino Installer Engine
+[assembly: AssemblyInformationalVersion("2")]
---
+++ RhinoFileVersionPlugIn.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using Rhino;
+using Rhino.FileIO;
+using Rhino.PlugIns;
+
+namespace RhinoFileVersion
+{
+  /// <summary>
+  /// RhinoFileVersionPlugIn plug-in class
+  /// </summary>
+  public class RhinoFileVersionPlugIn : PlugIn
+  {
+    /// <summary>
+    /// PlugIn.PlugInLoadTime override
+    /// </summary>
+    public override PlugInLoadTime LoadTime => PlugInLoadTime.AtStartup;
+
+    /// <summary>
+    /// PlugIn.OnLoad override
+    /// </summary>
+    protected override LoadReturnCode OnLoad(ref string errorMessage)
+    {
+      RhinoDoc.BeginOpenDocument += OnBeginOpenDocument;
+      return LoadReturnCode.Success;
+    }
+
+    /// <summary>
+    /// Called when Rhino begins to open a new document
+    /// </summary>
+    protected void OnBeginOpenDocument(object sender, DocumentOpenEventArgs e)
+    {
+      if (string.IsNullOrEmpty(e.FileName))
+        return;
+
+      var extension = Path.GetExtension(e.FileName);
+      if (!extension.Equals(".3dm") && !extension.Equals(".3dmbak"))
+        return;
+
+      if (!File.Exists(e.FileName))
+        return;
+
+      var action = e.Merge ? "Importing" : "Opening";
+      string version;
+
+      var archive_version = File3dm.ReadArchiveVersion(e.FileName);
+      switch (archive_version)
+      {
+        case 1:
+          version = "Rhino 1.0";
+          break;
+        case 2:
+          version = "Rhino 2.0";
+          break;
+        case 3:
+          version = "Rhino 3.0";
+          break;
+        case 4:
+          version = "Rhino 4.0";
+          break;
+        case 50:
+          version = "Rhino 5";
+          break;
+        default:
+          version = "unknown Rhino";
+          break;
+      }
+
+      RhinoApp.Write("\n{0} a {1} format file.\n", action, version);
+    }
+  }
+}
